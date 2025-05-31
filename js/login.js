b6dce00023da81b927d7a6759ac0d655d8a2d2c6
Luke Eastwood <lukeeastwood080506@gmail.com>
2025-05-31 19:55:54 +0100

    generational lock in

--- js/login.js
+++ js/login.js
@@ -1,22 +1,23 @@
-const emailInput = document.getElementById("email-input");
-const passwordInput = document.getElementById("password-input");
-
 const tempEmail = "admin@gmail.com";
 const tempPassword = "password";
 
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+const loginButton = document.getElementById("login-button");
+
 function submitLogin() {
-  // if the username and password match the temporary values, redirect to the home page.
-  if (
-    userNameInput.value === tempUsername &&
-    passwordInput.value === tempPassword
-  ) {
+  console.log("Login attempted with:", emailInput.value);
+  if (emailInput.value === tempEmail && passwordInput.value === tempPassword) {
     alert("Login successful!");
     sessionStorage.setItem("isLoggedIn", "true");
     window.location.href = "mainpg.html";
   } else {
     alert("Invalid username or password. Please try again.");
     sessionStorage.setItem("isLoggedIn", "false");
-    userNameInput.value = "";
+    emailInput.value = "";
     passwordInput.value = "";
   }
 }
+
+// Move the event listener inside DOMContentLoaded
+loginButton.addEventListener("click", submitLogin);
