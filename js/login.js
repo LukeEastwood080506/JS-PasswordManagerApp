9b72a55a779b87bfbff246f1fbe49924ef0b250b
Carl Baines <carltbaines@hotmail.co.uk>
2025-06-03 17:02:23 +0100

    Settings Page Responsiveness, JS + Login Validation Tweaks

--- js/login.js
+++ js/login.js
@@ -20,10 +20,15 @@ function submitLogin(){
     // if the email and password match the temporary values, redirect to the home page.
     // Alternatively, if the detailsCheck method returns true, redirect to the home page.
     // If true is returned from the detailsCheck method it means that the inputted details match those that are stored in local storage (user details that were submitted on sign-up).
-    if(emailInput.value === tempEmail && passwordInput.value === tempPassword || detailsCheck()){
+    if(emailInput.value.trim() === tempEmail && passwordInput.value.trim() === tempPassword || detailsCheck()){
         alert("Login successful!");
+        
+        // Store the current user's email for use in other pages
+        localStorage.setItem("currentUser", emailInput.value);
+        console.log(localStorage);
+        
         // Add link to the home page upon completion.
-        window.location.href = "#";
+        // window.location.href = "mainpg.html";
     }
     // Check that verifies whether an email was actually entered.
     // It checks for an @ symbol in the email input.
@@ -49,9 +54,7 @@ function detailsCheck(){
         // parses JSON string into an object
         const storedUserData = JSON.parse(storedUserJSON);
         // Compare the entered password with the stored password (the value from the object) in localStorage.
-        if(storedUserData.password === passwordInput.value){
-            emailInput.value = "";
-            passwordInput.value = "";
+        if(storedUserData.password === passwordInput.value.trim()){
             return true;
         }
         return false;
