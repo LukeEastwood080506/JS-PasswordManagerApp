98086fa3df9ef9f224119d5be846d4627bee74bb
Carl Baines <carltbaines@hotmail.co.uk>
2025-07-30 20:49:10 +0100

    Error Handling, Database Restructure, Tweaks

--- frontend/js/mainpg.js
+++ frontend/js/mainpg.js
@@ -1,14 +1,11 @@
-let currentPageOption = "menu-option-myvault";
+let currentPageOption = "myvault-link";
 let currentPage = "myvault-page";
 let accounts = [];
 
 let isEditMode = false;
 let originalAccount = null;
 
-const addPasswordModal = document.getElementById("add-password-modal");
-const addButton = document.getElementById("add-password-button");
-const modalCloseButton = document.getElementsByClassName("close")[0];
-
+// Account class
 class Account {
   constructor(service, email, password) {
     this.service = service;
@@ -17,95 +14,26 @@ class Account {
   }
 }
 
-function showPassword(service, email, password){
-  fetch("http://localhost:6969/passwords/show", {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ service, email, password })
-  })
-  .then((response) => response.json())
-  .then((data) =>{
-    if(data.success){
-      alert("Password has been displayed!");
-      const password = data.message;
-      const passwordElement = document.getElementById("account-password");
-      // const accountElement = document.getElementById("account");
-      if(passwordElement){
-        passwordElement.textContent = password;
-        passwordElement.style.display = "block";
-        // accountElement.style.display = "block";
-      }
-    }
-    else{
-      alert(data.message || "Password cannot be displayed");
-    }
-  })
-  .catch((error) =>{
-    alert(error.message);
-  })
-}
+// DOM elements
+const vaultLink = document.getElementById("myvault-link");
+const addPasswordModal = document.getElementById("add-password-container");
+const editPasswordModal = document.getElementById("edit-password-container");
 
-function save(service, email, password) {
-  fetch("http://localhost:6969/passwords/new", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ service, email, password }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        alert("Password created and added to vault successfully!");
-      } else {
-        alert(data.message || "Password Creation Unsuccessful!");
-      }
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-}
+const addModalOpenButton = document.getElementById("add-password-button");
+const modalAddPasswordBtn = document.getElementById("modal-add-button");
+const addModalCloseButton = document.getElementById("add-close-button");
+const editModalCloseButton = document.getElementById("edit-close-button");
 
-function deleteAccount(service, email, password) {
-  fetch("http://localhost:6969/passwords/delete", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ service, email, password }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        alert("Password deleted from vault successfully!");
-        refreshDiv(service, email);
-      } else {
-        alert(data.message || "Password Deletion Unsuccessful!");
-      }
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-}
+const passwordVisibilityIcon = document.querySelector(".view-password-icon");
 
 const PAGE_MAPPING = {
-  "menu-option-myvault": "myvault-page",
-  "menu-option-generator": "generator-page",
-  "menu-option-recyclebin": "recyclebin-page",
+  "myvault-link": "myvault-page",
+  "generator-link": "generator-page",
+  "recycle-bin-link": "recycle-bin-page",
 };
 
-// function checkLoggedIn() {
-//   if (sessionStorage.getItem("isLoggedIn") === "true") {
-//     initializeApp();
-//   } else {
-//     window.location.href = "loginpg.html";
-//   }
-// }
-
-function initializeApp() {
-  accounts.length = 0;
+function initialiseApp() {
+  console.log("Accounts: ", accounts);
   fetch("http://localhost:6969/passwords/all")
     .then((response) => response.json())
     .then((data) => {
@@ -115,13 +43,7 @@ function initializeApp() {
             const existing = accounts.find(
               (a) => a.service === acc.service && a.email === acc.email
             );
-            if(existing){
-              // Keeps the plaintext password stored in memory.
-              accounts.push(existing);
-            }
-            else{
-              console.warn("Skpping backend account due to original password being missing", acc);
-            }
+            accounts.push(acc);
           }
         });
         fillAccounts();
@@ -130,92 +52,51 @@ function initializeApp() {
       }
     })
     .catch((err) => {
+      alert("Error fetching passwords: ", err);
       console.error("Error fetching passwords:", err);
     });
   loadPage(currentPage, currentPageOption);
 }
 
-function refreshDiv(service, email) {
-  for (let i = 0; i < accounts.length; i++) {
-    if (accounts[i].service === service && accounts[i].email === email) {
-      accounts.splice(i, 1);
-      break;
+function fillAccounts() {
+  const accountsContainer = document.getElementById("password-records-section");
+  const template = document.querySelector(".account.template");
+
+  // Clear existing accounts
+  const existingAccounts = accountsContainer.querySelectorAll(".account:not(.template)");
+  existingAccounts.forEach((acc) => acc.remove());
+
+  accounts.forEach((account) => {
+    if (!account?.service || !account?.email || !account?.password) {
+      console.warn("Invalid account skipped:", account);
+      return;
     }
-  }
-  fillAccounts();
-}
 
-function loadPage(page, pageOption) {
-  const pageElement = document.getElementById(page);
-  const optionElement = document.getElementById(pageOption);
+    const clone = template.cloneNode(true);
+    clone.classList.remove("template");
+    clone.style.display = "flex";
 
-  if (pageElement && optionElement) {
-    optionElement.classList.add("active");
-    pageElement.style.display = "block";
-  }
-}
+    // Fill data
+    clone.querySelector(".account-service").textContent = account.service;
+    clone.querySelector(".account-email").textContent = account.email;
+    const passwordElement = clone.querySelector(".account-display-password");
 
-function changePage(pageOption) {
-  console.log("Changing to page:", pageOption);
+    clone.querySelector(".view-password-icon").addEventListener("click", () => {
+      showPassword(account.service, account.email, account.password);
+    });
 
-  if (currentPageOption === pageOption) {
-    return;
-  }
+    clone.querySelector(".edit-password-icon").addEventListener("click", () => {
+      showEditModal(account);
+    });
 
-  const currentOptionElement = document.getElementById(currentPageOption);
-  const currentPageElement = document.getElementById(currentPage);
+    clone.querySelector(".delete-password-icon").addEventListener("click", () => {
+      deleteAccount(account.service, account.email, account.password);
+    });
 
-  if (currentOptionElement) currentOptionElement.classList.remove("active");
-  if (currentPageElement) currentPageElement.style.display = "none";
-
-  currentPageOption = pageOption;
-  currentPage = PAGE_MAPPING[pageOption] || currentPage;
-
-  loadPage(currentPage, currentPageOption);
-}
-
-function showModal(account = null) {
-  const isEditing = account !== null;
-  isEditMode = isEditing;
-  originalAccount = isEditing ? account : null;
-
-  addPasswordModal.style.display = "block";
-  const title = document.getElementById("modal-title");
-  const button = document.getElementById("modal-add-button");
-
-  if (isEditing) {
-    title.textContent = "Edit Password";
-    button.innerHTML = `<span class = "plus">&#9998;</span><span>Update</span>`;
-
-    document.getElementById("add-service-input").value = account.service;
-    document.getElementById("add-email-input").value = account.email;
-    document.getElementById("add-password-input").value = account.password;
-  } else {
-    title.textContent = "Add New Password";
-    button.innerHTML = `<span class="plus">+</span><span>Add</span>`;
-    clearModalInputs();
-  }
-}
-
-function hideModal() {
-  addPasswordModal.style.display = "none";
-}
-
-function clearModalInputs() {
-  const inputs = [
-    document.getElementById("add-service-input"),
-    document.getElementById("add-email-input"),
-    document.getElementById("add-password-input"),
-  ];
-
-  inputs.forEach((input) => {
-    if (input) input.value = "";
+    accountsContainer.appendChild(clone);
   });
 }
 
-// function getFaviconUrl(domain) {
-//   return `https://www.google.com/s2/favicons?domain=${domain}&sz=256`;
-// }
 
 function handleAccountSubmit() {
   const service = document.getElementById("add-service-input").value.trim();
@@ -273,80 +154,192 @@ function handleAccountSubmit() {
   }
   isEditMode = false;
   originalAccount = null;
-  hideModal();
-  clearModalInputs();
+  hideAddModal();
+  clearAddModalInputs();
 }
 
-function fillAccounts() {
-  const accountsContainer = document.getElementById("accounts-section");
-
-  const existingAccounts = accountsContainer.querySelectorAll(
-    ".account:not(#account)"
-  );
-  existingAccounts.forEach((acc) => acc.remove());
-
-  console.log("Filling accounts:", accounts);
-  accounts.forEach((account, index) => {
-    if (!account || !account.service || !account.email || !account.password) {
-      console.warn("Invalid account skipped:", account);
-      return;
-    }
-
-    const template = document.getElementById("account");
-    const clone = template.cloneNode(true);
-    clone.style.display = "flex";
-    clone.removeAttribute("id");
-
-    clone.querySelector(".account-service").textContent = account.service;
-    clone.querySelector(".account-email").textContent = account.email;
-    // clone.querySelector(".account-icon").src = getFaviconUrl(account.service);
-
-    clone
-      .querySelector(".eye-icon")
-      .addEventListener("click", () =>
-        showPassword(account.service, account.email, account.password)
-      );
-    clone
-      .querySelector(".edit-icon")
-      .addEventListener("click", () => showModal(account));
-    clone
-      .querySelector(".delete-icon")
-      .addEventListener("click", () =>
-        deleteAccount(account.service, account.email, account.password)
-      );
-
-    accountsContainer.appendChild(clone);
-  });
+function showPassword(service, email, password) {
+  fetch("http://localhost:6969/passwords/show", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ service, email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("Password has been displayed!");
+        const passwordElement = document.querySelector(".account-display-password");
+        if (passwordElement) {
+          passwordElement.textContent = data.message;
+          passwordElement.style.display = "block";
+        }
+      } else {
+        alert(data.message || "Password cannot be displayed");
+      }
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 }
 
-// function editAccount(index) {
-//   const acc = accounts[index];
-//   if(!acc){
-//     console.error("Invalid account index:", index, accounts);
-//     return;
-//   }
-//   showModal(acc);
-// }
+function alternateEyeIcons(){
+  passwordVisibilityIcon.src = "eye-icon.svg";
+}
+
+function save(service, email, password) {
+  fetch("http://localhost:6969/passwords/new", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ service, email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("Password created and added to vault successfully!");
+      } else {
+        alert(data.message || "Password Creation Unsuccessful!");
+      }
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function deleteAccount(service, email, password) {
+  fetch("http://localhost:6969/passwords/delete", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ service, email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("Password deleted from vault successfully!");
+        refreshDiv(service, email);
+      } else {
+        alert(data.message || "Password Deletion Unsuccessful!");
+      }
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function loadPage(page, pageOption) {
+  const pageElement = document.getElementById(page);
+  const optionElement = document.getElementById(pageOption);
+
+  if (pageElement && optionElement) {
+    // Adds active to the class list - enabling the :active styling.
+    optionElement.classList.add("active");
+    // Displays the elements of the page which were previously hidden.
+    pageElement.style.display = "block";
+  }
+}
+
+function changePage(pageOption) {
+  if (currentPageOption === pageOption) {
+    // Page doesnt need changing.
+    return;
+  }
+  // Gets the current page option and page.
+  const currentOptionElement = document.getElementById(currentPageOption);
+  const currentPageElement = document.getElementById(currentPage);
+  if (currentOptionElement) {
+    // If the page option exists, active is removed from the class list - disabling the :active styling.
+    currentOptionElement.classList.remove("active");
+  }
+  if (currentPageElement) {
+    /// If the page exists, it is hidden to facilitate the page change.
+    currentPageElement.style.display = "none";
+  }
+  // The current page option is set to the page option parameter passed in.
+  currentPageOption = pageOption;
+  // The current page becomes the page that is found in the page mapping object at the index of the page option.
+  currentPage = PAGE_MAPPING[pageOption] || currentPage;
+  loadPage(currentPage, currentPageOption);
+}
 
 function logOut() {
-  console.log("Logging out user");
-  sessionStorage.setItem("isLoggedIn", "false");
+  // Need to add a feature that physically logs the user out as well as the redirect.
+  // so that the user can't click on the forward arrow to get back in.
   window.location.href = "loginpg.html";
 }
 
-if (addButton) {
-  addButton.addEventListener("click", showModal());
+function refreshDiv(service, email) {
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i].service === service && accounts[i].email === email) {
+      accounts.splice(i, 1);
+      break;
+    }
+  }
+  fillAccounts();
 }
 
-if (modalCloseButton) {
-  modalCloseButton.addEventListener("click", hideModal);
+/* Add Modal Methods */
+function showAddModal() {
+  clearAddModalInputs();
+  document.getElementById("modal-title").textContent = "Add New Password";
+  modalAddPasswordBtn.innerHTML = `<span class="plus">+</span><span>Add</span>`;
+  addPasswordModal.style.display = "block";
+}
+
+function hideAddModal() {
+  addPasswordModal.style.display = "none";
+}
+
+function clearAddModalInputs() {
+  document.getElementById("add-service-input").value = "";
+  document.getElementById("add-email-input").value = "";
+  document.getElementById("add-password-input").value = "";
+}
+
+/* Edit Modal Methods */
+function showEditModal(account){
+  if(!account){
+    return;
+  }
+  originalAccount = account;
+  document.getElementById("edit-modal-title").textContent = "Edit Password";
+  document.getElementById("edit-service-input").value = account.service;
+  document.getElementById("edit-email-input").value = account.email;
+  document.getElementById("edit-password-input").value = account.password;
+  editPasswordModal.style.display = "block";
+}
+
+function hideEditModal() {
+  editPasswordModal.style.display = "none";
+}
+
+/* Event Listeners */
+if (addModalOpenButton) {
+  addModalOpenButton.addEventListener("click", showAddModal);
+}
+
+if (addModalCloseButton) {
+  addModalCloseButton.addEventListener("click", hideAddModal);
+}
+
+if(passwordVisibilityIcon){
+  passwordVisibilityIcon.addEventListener("click", alternateEyeIcons)
 }
 
 window.addEventListener("click", (event) => {
   if (event.target === addPasswordModal) {
-    hideModal();
+    hideAddModal();
   }
 });
+
+document.querySelector(".pass-inputs").addEventListener("submit", (e) =>{
+  e.preventDefault();
+  handleAccountSubmit();
+})
 
 document.addEventListener("keydown", function (event) {
   const activeElement = document.activeElement;
@@ -366,10 +359,8 @@ document.addEventListener("keydown", function (event) {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  initializeApp();
-  if(addButton){
-    addButton.addEventListener("click", () => showModal());
+  initialiseApp();
+  if (addModalOpenButton) {
+    addModalOpenButton.addEventListener("click", () => showAddModal());
   }
 });
-
-// window.addEventListener("load", checkLoggedIn);
